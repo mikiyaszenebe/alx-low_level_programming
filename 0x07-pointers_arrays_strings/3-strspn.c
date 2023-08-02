05c6a7c5783956c002b38916ae799804977f8811
mikiyaszenebe <mikiyaszenebe@gmail.com>
2023-08-01 17:57:03 -0700

    task 3. strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,10 +1,12 @@
 #include "main.h"
+
 /**
 * _strspn - Entry point
 * @s: input
 * @accept: input
 * Return: Always 0 (Success)
 */
+
 unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int n = 0;
