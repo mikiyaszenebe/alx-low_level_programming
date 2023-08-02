c0e5a1485748ae34d55ce019b0176841018d7740
mikiyaszenebe <mikiyaszenebe@gmail.com>
2023-08-01 18:28:53 -0700

    task 6. Chess is mental torture

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -15,7 +15,8 @@ void print_chessboard(char (*a)[8])
 	for (i = 0; i < 8; i++)
 	{
 		for (j = 0; j < 8; j++)
-		       	putchar(a[i][j]);
-	       	putchar('\n');
+		{
+			putchar(a[i][j]);
+			putchar('\n');
+		}
 	}
-}
