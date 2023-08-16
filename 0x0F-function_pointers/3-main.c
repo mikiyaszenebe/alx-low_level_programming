f0694dd6aed18399361e054126fffd6c842a27e8
mikiyaszenebe <mikiyaszenebe@gmail.com>
2023-08-16 11:27:39 -0700

    task 3. A goal is not always meant to be reached, it often serves simply as something to aim at

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,49 +1,36 @@
-#include "function_pointers.h"
-	#include <stdlib.h>
-	#include <stdio.h>
-	#include "3-calc.h"
-	/**
-	 * main - Prints the result of simple operations.
-	 * @argc: The number of arguments supplied to the program.
-	 * @argv: An array of pointers to the arguments.
-	 *
-	 * Return: Always 0.
-	 */
-	int main(int __attribute__((__unused__)) argc, char *argv[])
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints its own opcodes
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int bytes, i;
+	char *arr;
+
+	if (argc != 2)
 	{
-		int num1, num2;
-		char *op;
-
-
-		if (argc != 4)
-		{
-			printf("Error\n");
-			exit(98);
-		}
-
-
-		num1 = atoi(argv[1]);
-		op = argv[2];
-		num2 = atoi(argv[3]);
-
-
-		if (get_op_func(op) == NULL || op[1] != '\0')
-		{
-			printf("Error\n");
-			exit(99);
-		}
-
-
-		if ((*op == '/' && num2 == 0) ||
-		    (*op == '%' && num2 == 0))
-		{
-			printf("Error\n");
-			exit(100);
-		}
-
-
-		printf("%d\n", get_op_func(op)(num1, num2));
-
-
-		return (0);
+	printf("Error\n");
+	exit(1);
 	}
+	bytes = atoi(argv[1]);
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	arr = (char *)main;
+	for (i = 0; i < bytes; i++)
+	{
+		if (i == bytes - 1)
+		
+			printf("%02hhx\n", arr[i]);
+		break;
+	}
+	printf("%02hhx ", arr[i]);
+	return (0);
+}
